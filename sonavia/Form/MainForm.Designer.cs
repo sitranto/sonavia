70dc20a23a228e0f36ce4e79a90545a1c012bd03
sitranto <89784211+sitranto@users.noreply.github.com>
2025-03-27 01:32:44 +0300

    added base ui

---
+++ sonavia/Form/MainForm.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace sonavia.Form
+{
+    partial class MainForm : System.Windows.Forms.Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges17 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            PanelMain = new Guna.UI2.WinForms.Guna2Panel();
+            ButtonLibrary = new Guna.UI2.WinForms.Guna2Button();
+            BuutonPerformers = new Guna.UI2.WinForms.Guna2Button();
+            ButtonAlbums = new Guna.UI2.WinForms.Guna2Button();
+            PanelMulti = new Guna.UI2.WinForms.Guna2Panel();
+            ButtonQueue = new Guna.UI2.WinForms.Guna2Button();
+            BuutonAbout = new Guna.UI2.WinForms.Guna2Button();
+            PanelPlaylist = new Guna.UI2.WinForms.Guna2Panel();
+            PanelTop = new Panel();
+            ButtonClose = new Guna.UI2.WinForms.Guna2Button();
+            PanelPlayer = new Panel();
+            PanelMain.SuspendLayout();
+            PanelMulti.SuspendLayout();
+            PanelTop.SuspendLayout();
+            SuspendLayout();
+            // 
+            // PanelMain
+            // 
+            PanelMain.BorderRadius = 12;
+            PanelMain.Controls.Add(ButtonLibrary);
+            PanelMain.Controls.Add(BuutonPerformers);
+            PanelMain.Controls.Add(ButtonAlbums);
+            PanelMain.CustomizableEdges = customizableEdges7;
+            PanelMain.FillColor = Color.FromArgb(39, 36, 36);
+            PanelMain.Location = new Point(64, 41);
+            PanelMain.Margin = new Padding(0);
+            PanelMain.Name = "PanelMain";
+            PanelMain.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            PanelMain.Size = new Size(586, 407);
+            PanelMain.TabIndex = 0;
+            // 
+            // ButtonLibrary
+            // 
+            ButtonLibrary.BackColor = Color.FromArgb(39, 36, 36);
+            ButtonLibrary.BorderRadius = 12;
+            ButtonLibrary.CustomizableEdges = customizableEdges1;
+            ButtonLibrary.DisabledState.BorderColor = Color.DarkGray;
+            ButtonLibrary.DisabledState.CustomBorderColor = Color.DarkGray;
+            ButtonLibrary.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            ButtonLibrary.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            ButtonLibrary.FillColor = Color.FromArgb(112, 103, 103);
+            ButtonLibrary.Font = new Font("Courier New", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            ButtonLibrary.ForeColor = Color.White;
+            ButtonLibrary.Location = new Point(11, 14);
+            ButtonLibrary.Margin = new Padding(0);
+            ButtonLibrary.Name = "ButtonLibrary";
+            ButtonLibrary.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            ButtonLibrary.Size = new Size(115, 25);
+            ButtonLibrary.TabIndex = 2;
+            ButtonLibrary.Text = "Библиотека";
+            // 
+            // BuutonPerformers
+            // 
+            BuutonPerformers.BackColor = Color.FromArgb(39, 36, 36);
+            BuutonPerformers.BorderRadius = 12;
+            BuutonPerformers.CustomizableEdges = customizableEdges3;
+            BuutonPerformers.DisabledState.BorderColor = Color.DarkGray;
+            BuutonPerformers.DisabledState.CustomBorderColor = Color.DarkGray;
+            BuutonPerformers.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            BuutonPerformers.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            BuutonPerformers.FillColor = Color.FromArgb(112, 103, 103);
+            BuutonPerformers.Font = new Font("Courier New", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            BuutonPerformers.ForeColor = Color.White;
+            BuutonPerformers.Location = new Point(223, 14);
+            BuutonPerformers.Margin = new Padding(0);
+            BuutonPerformers.Name = "BuutonPerformers";
+            BuutonPerformers.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            BuutonPerformers.Size = new Size(115, 25);
+            BuutonPerformers.TabIndex = 1;
+            BuutonPerformers.Text = "Исполнители";
+            // 
+            // ButtonAlbums
+            // 
+            ButtonAlbums.BackColor = Color.FromArgb(39, 36, 36);
+            ButtonAlbums.BorderRadius = 12;
+            ButtonAlbums.CustomizableEdges = customizableEdges5;
+            ButtonAlbums.DisabledState.BorderColor = Color.DarkGray;
+            ButtonAlbums.DisabledState.CustomBorderColor = Color.DarkGray;
+            ButtonAlbums.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            ButtonAlbums.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            ButtonAlbums.FillColor = Color.FromArgb(112, 103, 103);
+            ButtonAlbums.Font = new Font("Courier New", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            ButtonAlbums.ForeColor = Color.White;
+            ButtonAlbums.Location = new Point(132, 14);
+            ButtonAlbums.Margin = new Padding(0);
+            ButtonAlbums.Name = "ButtonAlbums";
+            ButtonAlbums.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            ButtonAlbums.Size = new Size(85, 25);
+            ButtonAlbums.TabIndex = 0;
+            ButtonAlbums.Text = "Альбомы";
+            // 
+            // PanelMulti
+            // 
+            PanelMulti.BorderRadius = 12;
+            PanelMulti.Controls.Add(ButtonQueue);
+            PanelMulti.Controls.Add(BuutonAbout);
+            PanelMulti.CustomizableEdges = customizableEdges13;
+            PanelMulti.FillColor = Color.FromArgb(39, 36, 36);
+            PanelMulti.Location = new Point(657, 41);
+            PanelMulti.Margin = new Padding(0);
+            PanelMulti.Name = "PanelMulti";
+            PanelMulti.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            PanelMulti.Size = new Size(250, 407);
+            PanelMulti.TabIndex = 1;
+            // 
+            // ButtonQueue
+            // 
+            ButtonQueue.BackColor = Color.FromArgb(39, 36, 36);
+            ButtonQueue.BorderRadius = 12;
+            ButtonQueue.CustomizableEdges = customizableEdges9;
+            ButtonQueue.DisabledState.BorderColor = Color.DarkGray;
+            ButtonQueue.DisabledState.CustomBorderColor = Color.DarkGray;
+            ButtonQueue.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            ButtonQueue.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            ButtonQueue.FillColor = Color.FromArgb(112, 103, 103);
+            ButtonQueue.Font = new Font("Courier New", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            ButtonQueue.ForeColor = Color.White;
+            ButtonQueue.Location = new Point(11, 14);
+            ButtonQueue.Margin = new Padding(0);
+            ButtonQueue.Name = "ButtonQueue";
+            ButtonQueue.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            ButtonQueue.Size = new Size(85, 25);
+            ButtonQueue.TabIndex = 4;
+            ButtonQueue.Text = "Очередь";
+            // 
+            // BuutonAbout
+            // 
+            BuutonAbout.BackColor = Color.FromArgb(39, 36, 36);
+            BuutonAbout.BorderRadius = 12;
+            BuutonAbout.CustomizableEdges = customizableEdges11;
+            BuutonAbout.DisabledState.BorderColor = Color.DarkGray;
+            BuutonAbout.DisabledState.CustomBorderColor = Color.DarkGray;
+            BuutonAbout.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            BuutonAbout.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            BuutonAbout.FillColor = Color.FromArgb(112, 103, 103);
+            BuutonAbout.Font = new Font("Courier New", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            BuutonAbout.ForeColor = Color.White;
+            BuutonAbout.Location = new Point(102, 14);
+            BuutonAbout.Margin = new Padding(0);
+            BuutonAbout.Name = "BuutonAbout";
+            BuutonAbout.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            BuutonAbout.Size = new Size(115, 25);
+            BuutonAbout.TabIndex = 3;
+            BuutonAbout.Text = "О текущем";
+            // 
+            // PanelPlaylist
+            // 
+            PanelPlaylist.BorderRadius = 12;
+            PanelPlaylist.CustomizableEdges = customizableEdges15;
+            PanelPlaylist.FillColor = Color.FromArgb(39, 36, 36);
+            PanelPlaylist.Location = new Point(7, 41);
+            PanelPlaylist.Margin = new Padding(0);
+            PanelPlaylist.Name = "PanelPlaylist";
+            PanelPlaylist.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            PanelPlaylist.Size = new Size(50, 407);
+            PanelPlaylist.TabIndex = 2;
+            // 
+            // PanelTop
+            // 
+            PanelTop.Controls.Add(ButtonClose);
+            PanelTop.Location = new Point(0, 0);
+            PanelTop.Margin = new Padding(0);
+            PanelTop.Name = "PanelTop";
+            PanelTop.Size = new Size(914, 41);
+            PanelTop.TabIndex = 3;
+            PanelTop.MouseDown += PanelTop_MouseDown;
+            PanelTop.MouseMove += PanelTop_MouseMove;
+            PanelTop.MouseUp += PanelTop_MouseUp;
+            // 
+            // ButtonClose
+            // 
+            ButtonClose.CustomizableEdges = customizableEdges17;
+            ButtonClose.DisabledState.BorderColor = Color.DarkGray;
+            ButtonClose.DisabledState.CustomBorderColor = Color.DarkGray;
+            ButtonClose.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            ButtonClose.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            ButtonClose.FillColor = Color.FromArgb(22, 22, 22);
+            ButtonClose.Font = new Font("Segoe UI", 9F);
+            ButtonClose.ForeColor = Color.White;
+            ButtonClose.Location = new Point(877, 5);
+            ButtonClose.Name = "ButtonClose";
+            ButtonClose.ShadowDecoration.CustomizableEdges = customizableEdges18;
+            ButtonClose.Size = new Size(30, 30);
+            ButtonClose.TabIndex = 0;
+            ButtonClose.Text = "X";
+            ButtonClose.Click += ButtonClose_Click;
+            // 
+            // PanelPlayer
+            // 
+            PanelPlayer.Location = new Point(7, 456);
+            PanelPlayer.Name = "PanelPlayer";
+            PanelPlayer.Size = new Size(900, 48);
+            PanelPlayer.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(22, 22, 22);
+            ClientSize = new Size(914, 513);
+            Controls.Add(PanelPlayer);
+            Controls.Add(PanelTop);
+            Controls.Add(PanelPlaylist);
+            Controls.Add(PanelMulti);
+            Controls.Add(PanelMain);
+            Font = new Font("Courier New", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "MainForm";
+            Text = "MainForm";
+            PanelMain.ResumeLayout(false);
+            PanelMulti.ResumeLayout(false);
+            PanelTop.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel PanelMain;
+        private Guna.UI2.WinForms.Guna2Panel PanelMulti;
+        private Guna.UI2.WinForms.Guna2Panel PanelPlaylist;
+        private Guna.UI2.WinForms.Guna2Button ButtonAlbums;
+        private Guna.UI2.WinForms.Guna2Button BuutonPerformers;
+        private Guna.UI2.WinForms.Guna2Button ButtonLibrary;
+        private Panel PanelTop;
+        private Guna.UI2.WinForms.Guna2Button ButtonQueue;
+        private Guna.UI2.WinForms.Guna2Button BuutonAbout;
+        private Panel PanelPlayer;
+        private Guna.UI2.WinForms.Guna2Button ButtonClose;
+    }
+}
---
+++ sonavia/Form/MainForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sonavia.Form
+{
+    public partial class MainForm : System.Windows.Forms.Form
+    {
+        private static bool isPanelTopMouseClicked = false;
+        private static Point formLocation;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            OnLoad();
+        }
+
+        private void OnLoad()
+        {
+            ButtonLibrary.FillColor = Color.FromArgb(231, 128, 32);
+        }
+
+        private void PanelTop_MouseDown(object sender, MouseEventArgs e)
+        {
+            isPanelTopMouseClicked = true;
+            formLocation = e.Location;
+        }
+
+        private void PanelTop_MouseUp(object sender, MouseEventArgs e)
+        {
+            isPanelTopMouseClicked = false;
+        }
+
+        private void PanelTop_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPanelTopMouseClicked)
+            {
+                Location = new Point(Location.X - formLocation.X + e.X, Location.Y - formLocation.Y + e.Y);
+                Update();
+            }
+        }
+
+        private void ButtonClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
